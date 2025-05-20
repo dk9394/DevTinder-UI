63e1d396f874ef02ab5e0f3f0a9ffa2536ad4eae
dk9394 <k.deepak9394@gmail.com>
2025-05-20 17:25:18 +0530

    Major UI changes

--- src/app/feeds/feeds.component.ts
+++ src/app/feeds/feeds.component.ts
@@ -14,12 +14,39 @@ import { selectFeeds } from '../store/feeds/feed.selectors';
 export class FeedsComponent implements OnInit {
   feeds: IUser[] = [];
 
+  currentUserIndex: number = 0;
+
+  feed!: IUser;
+
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.store.dispatch(addFeeds());
     this.store.select(selectFeeds).subscribe((feeds: IUser[]) => {
       this.feeds = feeds;
+      this.setFeed();
     });
+  }
+
+  setNextFeed(): void {
+    this.currentUserIndex++;
+    this.setFeed();
+  }
+
+  private setFeed() {
+    this.feed = this.setDefaultUser(this.feeds[this.currentUserIndex]);
+  }
+
+  private setDefaultUser(feed: IUser) {
+    const newUser = {
+      ...feed,
+      age: feed?.age || 30,
+      gender: feed?.gender || 'male',
+      about: feed?.about || 'I am 10 years experienced frontend engineer.',
+      skills: feed?.skills?.length
+        ? feed?.skills
+        : ['JavaScript', 'Angular', 'NodeJS', 'TypeScript'],
+    };
+    return newUser;
   }
 }
